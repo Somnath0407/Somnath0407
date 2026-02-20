7a3e81e74b0b85ade1696aaa2e34f861f7b0f540
Somnath0407 <somnathmondal2121@gmail.com>
2026-02-20 23:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (58.93%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int bestending = nums[0];
-        int badengind = nums[0];
-        int res = nums[0];
-
-        for(int i = 1; i < nums.size(); i++){
-            
-            int product = nums[i] * bestending;
-            int pro = nums[i] * badengind;
-            int orginal = nums[i];
-
-            int prevBest = bestending;
-            int prevBad = badengind;
-
-            bestending = max(orginal, max(product, pro));
-            badengind = min(orginal, min(product, pro));
-
-            res = max(res, bestending);
+        int bestending=nums[0];
+        int badengind=nums[0];
+        int res=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int product=nums[i]*bestending;
+            int pro=nums[i]*badengind;
+            int orginal=nums[i];
+            bestending=max(orginal,{max(product,pro)});
+            badengind=min(orginal,{min(product,pro)});
+            res=max(res,{max(badengind,bestending)});
         }
         return res;
     }
