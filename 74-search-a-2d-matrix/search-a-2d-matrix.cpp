c9a760e911c627a6a66991ac7fb05be177a1a739
Somnath0407 <somnathmondal2121@gmail.com>
2026-03-17 21:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (44.55%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -8,15 +8,9 @@ public:
             int mid = (low + high) / 2;
             int row = mid / m;
             int col = mid % m;
-            if (matrix[row][col] == target) {
-                return true;
-            }
-            else if (matrix[row][col] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
+            if (matrix[row][col] == target) return true;
+            else if (matrix[row][col] < target) low = mid + 1;
+            else high = mid - 1;
         }
         return false;
     }
