13d051b782bbab1e6172bc4329e1d8362b72e0e3
Somnath Mondal <somnathmondal2121@gmail.com>
2026-08-06 21:51:32 +0530

    Time: 31 ms (95.83%) | Memory: 45.4 MB (71.09%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>m;
-        int sum=0,res=0;
-        m[0]=1;
+        unordered_map<int,int>mp;
+        int presum=0;
+        int total=0;
+        mp[0]=1;
         for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            int question=sum-k;
-            int freq=m[question];
-            res+=freq;
-            m[sum]++;
+            presum+=nums[i];
+            if(mp.count(presum-k)){
+                total+=mp[presum-k];
+                mp[presum]++;
+            }
+            else{
+                mp[presum]++;
+            }
         }
-        return res;
+        return total;
     }
 };
